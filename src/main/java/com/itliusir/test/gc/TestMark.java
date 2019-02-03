1200e1f0d9a57f4b37fd5427876c25064322761f
itliusir <liugang@itliusir.com>
2019-02-03 11:28:48 +0800

    add sweeping test

--- src/main/java/com/itliusir/test/gc/TestMark.java
+++ src/main/java/com/itliusir/test/gc/TestMark.java
@@ -2,6 +2,9 @@ package com.itliusir.test.gc;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * 位图标记 Case
  *
@@ -14,19 +17,28 @@ public class TestMark {
 
     private static final long WORD_LENGTH = 1L;
     private static final long HEAP_START = 0L;
+    private static final long HEAP_END = 15L;
+    private static final long HEAP_SIZE = HEAP_END - HEAP_START + 1L;
     private static long[] BITMAP_TBL = new long[16];
 
     public static void main(String[] args) {
         TestMark testMark = new TestMark();
-        ChildObj childObj = new ChildObj();
-        childObj.setObjAddress(8L);
-        childObj.setObj("我是爸爸");
-        ChildObj childrenObj = new ChildObj();
-        childrenObj.setObjAddress(14L);
-        childrenObj.setObj("我是儿子");
-        childObj.setChildrenObj(childrenObj);
+        ChildObj one = new ChildObj();
+        one.setObjAddress(0L);
+        one.setObj("我是爷爷");
+        ChildObj two = new ChildObj();
+        two.setObjAddress(4L);
+        two.setObj("我是爸爸");
+        ChildObj three = new ChildObj();
+        three.setObjAddress(6L);
+        three.setObj("我是儿子");
+        two.setChildrenObj(three);
+        one.setChildrenObj(two);
+        // 位图标记
+        testMark.mark(one);
 
-        testMark.mark(childObj);
+        // 位图清理
+        testMark.sweepAndPhase();
     }
 
     /**
@@ -44,10 +56,34 @@ public class TestMark {
             if ((BITMAP_TBL[index] & (1 << offset)) == 0) {
                 // 如果相对应位都是0，则结果为0，否则为1
                 BITMAP_TBL[index] |= (1 << offset);
-                log.info("BITMAP_TBL------------>{}", BITMAP_TBL);
+                log.info("Marking--BITMAP_TBL------------>{}", BITMAP_TBL);
                 mark(children(obj));
             }
         }
+    }
+
+    /**
+     * 这里模拟 -1 代表清空
+     *
+     * @author liugang 2019/2/3 11:20
+     */
+    private void sweepAndPhase() {
+        long sweeping = HEAP_START;
+        int index = 0;
+        int offset = 0;
+        List<Integer> freeList = new LinkedList<>();
+        while (sweeping < HEAP_SIZE) {
+            if ((BITMAP_TBL[index] & (1 << offset)) == 0) {
+                freeList.add((int) sweeping);
+            }
+            index += (offset + 1) / WORD_LENGTH;
+            offset = (int) ((offset + 1) % WORD_LENGTH);
+            sweeping++;
+        }
+        for (int freeIndex : freeList) {
+            BITMAP_TBL[freeIndex] = -1;
+        }
+        log.info("Sweeping--BITMAP_TBL------------>{}", BITMAP_TBL);
     }
 
     private ChildObj children(IObject obj) {
